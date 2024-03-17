b26fa0ae74e99612527634c657d1c8b4ec9982af
Rking <120440@qq.com>
2024-03-17 18:12:17 +0800

    third

---
+++ first/scripts/main.js
@@ -0,0 +1,5 @@
+alert("Hello, you make me love and hate, panic and uneasy the world\n你好，你让我又爱又恨，恐慌而又不安的世界");
+document.querySelector("button").addEventListener("click", function () {
+    alert("别戳我，我怕疼。");
+  });
+  
